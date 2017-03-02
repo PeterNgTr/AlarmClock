6a1b7a640a11fca3f585b72317eabae1c2e90909
Thanh Nguyen Trung <thanh.nguyen-trung@hellopay.com>
2017-03-02 14:10:05 +0700

    adding notification

--- app/src/main/java/com/example/thanhnguyen/alarmclock/RingtonePlayingService.java
+++ app/src/main/java/com/example/thanhnguyen/alarmclock/RingtonePlayingService.java
@@ -1,11 +1,13 @@
 package com.example.thanhnguyen.alarmclock;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Created by thanhnguyen on 3/1/17.
@@ -40,6 +42,23 @@ public class RingtonePlayingService extends Service {
             mediaPlayer.start();
             this.isPlaying = true;
             this.startId = 0;
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            Intent intentMainActivity = new Intent(this.getApplicationContext(), MainActivity.class);
+
+            PendingIntent pendingIntentMainActivity = PendingIntent.getActivity(this,0,intentMainActivity,0);
+
+            Notification notifyPopup = new Notification.Builder(this)
+                    .setContentTitle("An alarm is going off!")
+                    .setContentText("Click me!")
+                    .setContentIntent(pendingIntentMainActivity)
+                    .setAutoCancel(true)
+                    .setSmallIcon(R.drawable.notifyicon)
+                    .build();
+
+            notificationManager.notify(0, notifyPopup);
+
         } else if (this.isPlaying && this.startId == 0) {
 
             mediaPlayer.stop();
