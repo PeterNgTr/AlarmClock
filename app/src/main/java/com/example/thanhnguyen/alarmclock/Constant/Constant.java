17f812ba2e94c0182ab88c55561c2e29bfa6612a
Thanh Nguyen Trung <thanh.nguyen-trung@hellopay.com>
2017-03-03 13:31:30 +0700

    refactoring code

---
+++ app/src/main/java/com/example/thanhnguyen/alarmclock/Constant/Constant.java
@@ -0,0 +1,21 @@
+package com.example.thanhnguyen.alarmclock.Constant;
+
+/**
+ * Created by thanhnguyen on 3/3/17.
+ */
+public class Constant {
+
+    public static final String STRING_COLON = ":";
+
+    public static final String KEY_EXTRA_INTENT = "extra";
+
+    public static final String VALUE_EXTRA_INTENT_ALARM_ON = "alarm on";
+
+    public static final String VALUE_EXTRA_INTENT_ALARM_OFF = "alarm off";
+
+    public static final String STRING_TIME_SET_IN_THE_PAST = "Current time is in the past. Please set another time";
+
+    public static final String STRING_ALARM_IS_OFF = "Alarm off!";
+
+    public static final String STRING_ALARM_IS_SET = "Alarm set to ";
+}
--- app/src/main/java/com/example/thanhnguyen/alarmclock/Receiver/AlarmReceiver.java
+++ app/src/main/java/com/example/thanhnguyen/alarmclock/Receiver/AlarmReceiver.java
@@ -1,8 +1,11 @@
-package com.example.thanhnguyen.alarmclock;
+package com.example.thanhnguyen.alarmclock.Receiver;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
+import com.example.thanhnguyen.alarmclock.Service.RingtonePlayingService;
+import com.example.thanhnguyen.alarmclock.Constant.*;
 
 /**
  * Created by thanhnguyen on 3/1/17.
@@ -12,11 +15,11 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        String getExtraString = intent.getExtras().getString("extra");
+        String getExtraString = intent.getExtras().getString(Constant.KEY_EXTRA_INTENT);
 
         Intent ringtoneServiceIntent = new Intent(context, RingtonePlayingService.class);
 
-        ringtoneServiceIntent.putExtra("extra", getExtraString);
+        ringtoneServiceIntent.putExtra(Constant.KEY_EXTRA_INTENT, getExtraString);
 
         context.startService(ringtoneServiceIntent);
     }
